c3bdf333fb25d6f134b236a2ae32ca40730b46d3
Santiago Quintero Uribe <santiquinterouribe0412@gmail.com>
2025-10-28 21:53:25 -0500

    first commit

---
+++ src/main/java/com/uniquindio/archmicroserv/gestion_perfil/GestionPerfilApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.archmicroserv.gestion_perfil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPerfilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPerfilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/archmicroserv/gestion_perfil/GestionPerfilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.archmicroserv.gestion_perfil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPerfilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
